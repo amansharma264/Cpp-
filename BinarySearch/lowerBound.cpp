fba4a37f2925bbfdc0336f16f4c4e5efc46b13f6
amansharma264 <amankumar170789@gmail.com>
2025-06-18 18:25:55 +0530

    Update array values and input parameters for lower bound binary search

--- BinarySearch/lowerBound.cpp
+++ BinarySearch/lowerBound.cpp
@@ -1,9 +1,9 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int arr[] = {1, 2, 4, 5 , 9, 15, 18, 21, 24};
-    int n = 9;
-    int x = 12;
+    int arr[] = {1, 3, 3, 4};
+    int n = 4;
+    int x = 3;
     // for(int i = 0; i<n ; i++){
     //     if(arr[i] > x){
     //         cout << arr[i-1];
