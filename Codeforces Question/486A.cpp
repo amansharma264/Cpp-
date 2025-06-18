5c6e121f3c15355133caed00cd51ae2647511086
amansharma264 <amankumar170789@gmail.com>
2025-06-19 00:06:12 +0530

    Refactor code structure for improved readability and maintainability

---
+++ Codeforces Question/486A.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    long long n;
+    cin >> n;
+
+    long long result;
+    if (n % 2 == 0) {
+        result = n / 2;
+    } else {
+        result = -((n + 1) / 2);
+    }
+
+    cout << result << endl;
+    return 0;
+}
