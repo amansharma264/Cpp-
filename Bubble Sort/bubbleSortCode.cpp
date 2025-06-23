f8e0a5b0592e8c220ea2193f8d55a66111692ba2
amansharma264 <amankumar170789@gmail.com>
2025-06-24 00:23:37 +0530

    Optimize Bubble Sort implementation with early exit condition

--- Bubble Sort/bubbleSortCode.cpp
+++ Bubble Sort/bubbleSortCode.cpp
@@ -9,15 +9,29 @@ int main(){
     cout << arr[i] <<" ";
    }
    // bubble sort
+//    for(int i = 0; i<n-1; i++){ // n-1 passes
+//     // traverse
+//     for(int j = 0; j<n-1-i; j++){
+//         if(arr[j] > arr[j+1]){ // swap
+//             // int temp = arr[j];
+//             // arr[j] = arr[j+1];
+//             // arr[j+1] = temp;
+//             swap(arr[j], arr[j+1]);
+//         }
+//     }
+//    }
+   // Bubble shot optimised
    for(int i = 0; i<n-1; i++){ // n-1 passes
     // traverse
+    bool flag = true;
     for(int j = 0; j<n-1-i; j++){
-        if(arr[j] > arr[j+1]){ // swap
-            // int temp = arr[j];
-            // arr[j] = arr[j+1];
-            // arr[j+1] = temp;
+        if(arr[j] > arr[j+1]){
             swap(arr[j], arr[j+1]);
+            flag = false;
         }
+    }
+    if(flag == true){ // swap didn't happen
+        break;
     }
    }
    cout << endl;
