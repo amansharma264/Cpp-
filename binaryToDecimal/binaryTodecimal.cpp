892cdb410a7d533256396802a7f22648421553ad
amansharma264 <amankumar170789@gmail.com>
2025-07-07 21:02:15 +0530

    Refactor code structure for improved readability and maintainability

--- binaryToDecimal/binaryTodecimal.cpp
+++ binaryToDecimal/binaryTodecimal.cpp
@@ -12,8 +12,24 @@ int binary_to_decimal(string &binary){
     return result;
 }
 
+string decimal_to_binary(int num){
+    string result = "";
+    while(num > 0){ // even
+        if(num % 2 == 0){
+            result = '0' + result;
+        }
+        else{ // odd
+            result = '1' + result;
+        }
+        num = num/2;
+    }
+    return result;
+}
+
+
 int main(){
     string str = "001101";
     cout << binary_to_decimal(str) << "\n";
+    cout << decimal_to_binary(13) << "\n";
     return 0;
 }
