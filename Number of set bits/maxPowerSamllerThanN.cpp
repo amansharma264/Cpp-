292d7652787367be49aaf3a44a3364819e98467d
amansharma264 <amankumar170789@gmail.com>
2025-07-07 21:35:57 +0530

    Refactor code structure for improved readability and maintainability

--- Number of set bits/maxPowerSamllerThanN.cpp
+++ Number of set bits/maxPowerSamllerThanN.cpp
@@ -1,5 +1,16 @@
 #include<iostream>
 using namespace std;
+
+int max_power_of2(int n){
+    n = n | (n >> 1);
+    n = n | (n >> 2);
+    n = n | (n >> 4);
+    n = n | (n >> 8);
+    n = n | (n >> 16);
+    n = n | (n >> 32);
+
+}
+
 int main(){
     int x = 24;
     int temp;
