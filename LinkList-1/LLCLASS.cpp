3a0765be888283e87064acdb7ff5c17a8c9b4776
amansharma264 <amankumar170789@gmail.com>
2025-07-16 23:34:31 +0530

    Refactor insert functions in linked list; rename insertAtEnd to insertAtTail and implement insertAtHead and insertAtIdx

--- LinkList-1/LLCLASS.cpp
+++ LinkList-1/LLCLASS.cpp
@@ -19,7 +19,7 @@ public:
         head = tail = NULL;
         size = 0;
     }
-    void insertAtEnd(int val){
+    void insertAtTail(int val){
         Node* temp = new Node(val);
         if(size == 0) head = tail = temp;
         else{
@@ -28,6 +28,33 @@ public:
         }
         size++;
     }
+
+    void insertAtHead(int val){
+        Node* temp = new Node(val);
+        if(size == 0) head = tail = temp;
+        else{
+            temp->next = head;
+            head = temp;
+        }
+        size++;
+    }
+
+    void insertAtIdx(int idx, int val){
+        if(idx < 0 || idx > size) cout << "Invalid index" << endl;
+        else if(idx == 0) insertAtHead(val);
+        else if(idx == size) insertAtTail(val);
+        else{
+            Node* t = new Node(val);
+            Node* temp = head;
+            for(int i = 1; i<= idx-1; i++){
+                temp = temp->next;
+            }
+            t->next = temp->next;
+            temp->next = t;
+            size++;
+        }
+    }
+
     void display(){
         Node* temp = head;
         while(temp!=NULL){
@@ -40,11 +67,17 @@ public:
 
 int main(){
     LinkList ll; //{ }
-    ll.insertAtEnd(10); // {10->NULL}
+    ll.insertAtTail(10); // {10->NULL}
     ll.display();
-    ll.insertAtEnd(20); //{10 -> 20->NULL}
+    ll.insertAtTail(20); //{10 -> 20->NULL}
     ll.display();
-    ll.insertAtEnd(30);
-    ll.insertAtEnd(40);
+    ll.insertAtTail(30); // {10->20->30->NULL}
+    ll.insertAtTail(40); // {10->20->30->40->NULL}
+    ll.display();
+    ll.insertAtHead(50);
+    ll.display();
+    ll.insertAtHead(24);
+    ll.display();
+    ll.insertAtIdx(4,80); 
     ll.display();
 }
