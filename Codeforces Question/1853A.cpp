054f473a4d8a96cbd09fa72fad01864beaf222db
amansharma264 <amankumar170789@gmail.com>
2025-10-07 15:45:05 +0530

    Implement code changes to enhance functionality and improve performance

--- Codeforces Question/1853A.cpp
+++ Codeforces Question/1853A.cpp
@@ -1,5 +1,33 @@
-#include<iostream>
+#include <iostream>
+#include <vector>
+#include<algorithm>
+#include<math.h>
 using namespace std;
-int main(){
-    
+
+void solve(){
+    long long n;
+    cin >> n;
+    vector<long long> arr(n);
+    for(int i = 0; i<n; i++){
+        cin >> arr[i];
+    }
+    long long diff = INT_MAX;
+    for(int i = 0; i<n-1; i++){
+        if(arr[i] > arr[i+1]){
+            cout << 0 << endl;
+            return;
+        }
+        else{
+            diff = min(diff, (arr[i+1] - arr[i]));
+        }
+    }
+    cout << diff/2 + 1 << endl;
+}
+
+int main() {
+    int t;
+    cin >> t;
+    while(t--){
+        solve();
+    }
 }
