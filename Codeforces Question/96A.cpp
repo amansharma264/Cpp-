9e8b9d75d67973770e4c9a5ea8d87e1dbb09b03c
amansharma264 <amankumar170789@gmail.com>
2025-10-23 23:30:37 +0530

    update cp code

---
+++ Codeforces Question/96A.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<string>
+#include<algorithm>
+using namespace std;
+int main(){
+    string s;
+    cin >> s;
+    int count = 1;
+    for (int i = 1; i < s.length(); ++i) {
+        if (s[i] == s[i-1]) count++;
+        else count = 1;
+        if (count >= 7) {
+            cout << "YES" << endl;
+            return 0;
+        }
+    }
+    cout << "NO" << endl;
+    return 0;
+}
