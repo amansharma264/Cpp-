ff0abea13a54dc93bb871f3bf10057f681cb6ced
amansharma264 <amankumar170789@gmail.com>
2025-11-26 15:20:12 +0530

    update cp

---
+++ Codeforces Question/1999A.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastio() ios::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr)
+#define ll long long
+#define endl '\n'
+
+int main(){
+    fastio();
+    ll t;
+    cin >> t;
+    while(t--){
+        ll n;
+        cin >> n;
+        cout << (n / 10 + n % 10) << '\n';
+    }
+}
