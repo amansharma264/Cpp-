f226e804806bed7b9a4206c8cd91a09ac6b9a5cd
amansharma264 <amankumar170789@gmail.com>
2026-02-19 23:47:02 +0530

    Refactor code structure for improved readability and maintainability

--- Arrays/sumofArrays.cpp
+++ Arrays/sumofArrays.cpp
@@ -1,11 +1,12 @@
 #include<iostream>
+#include<vector>
 using namespace std;
 int main(){
     int n;
     cout << "Enter size of array : ";
     cin >> n;
 
-    int arr[n];
+    vector<int> arr(n);
     // input
     for(int i = 0; i <= n-1; i++){
         cin >> arr[i];
